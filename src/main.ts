1013d36d23d8ada8693940e9cec8687a642350e8
titicodes <anietimfoneeffiong@gmail.com>
2025-05-01 13:07:28 +0100

    cors enable

--- src/main.ts
+++ src/main.ts
@@ -9,8 +9,11 @@ import { ENVIRONMENT } from './common/config/environment';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, {
-    cors: true,
+  const app = await NestFactory.create(AppModule)
+  app.enableCors({
+    origin: ['http://localhost:3000', ],
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
   });
 
   app.use(helmet());
